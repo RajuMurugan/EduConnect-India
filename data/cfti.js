eb734eebd65a04141e4bcda571754a5b5427b2d2
RajuMurugan <rajumurugannp@gmail.com>
2025-11-10 13:15:44 +0530

    Update cfti.js

--- data/cfti.js
+++ data/cfti.js
@@ -1,5 +1,63 @@
-
 const cftis = [
+  // IIEST
+  {name:'Indian Institute of Engineering Science and Technology, Shibpur', type:'CFTI', state:'West Bengal', city:'Howrah', website:'https://www.iiests.ac.in'},
+
+  // SPAs
+  {name:'School of Planning and Architecture, New Delhi', type:'CFTI', state:'Delhi', city:'New Delhi', website:'https://www.spa.ac.in'},
+  {name:'School of Planning and Architecture, Bhopal', type:'CFTI', state:'Madhya Pradesh', city:'Bhopal', website:'https://spabhopal.ac.in'},
+  {name:'School of Planning and Architecture, Vijayawada', type:'CFTI', state:'Andhra Pradesh', city:'Vijayawada', website:'https://spav.ac.in'},
+
+  // IIITDMs
+  {name:'Indian Institute of Information Technology, Design and Manufacturing, Kancheepuram', type:'CFTI', state:'Tamil Nadu', city:'Chennai', website:'https://www.iiitdm.ac.in'},
+  {name:'Indian Institute of Information Technology, Design and Manufacturing, Jabalpur', type:'CFTI', state:'Madhya Pradesh', city:'Jabalpur', website:'https://www.iiitdmj.ac.in'},
+  {name:'Indian Institute of Information Technology, Design and Manufacturing, Kurnool', type:'CFTI', state:'Andhra Pradesh', city:'Kurnool', website:'https://iiitk.ac.in'},
+
+  // NIAMT (formerly NIFFT)
+  {name:'National Institute of Advanced Manufacturing Technology (formerly NIFFT)', type:'CFTI', state:'Jharkhand', city:'Ranchi', website:'https://www.nifft.ac.in'},
+
+  // SLIET
+  {name:'Sant Longowal Institute of Engineering and Technology', type:'CFTI', state:'Punjab', city:'Longowal', website:'https://www.sliet.ac.in'},
+
+  // NERIST
+  {name:'North Eastern Regional Institute of Science and Technology', type:'CFTI', state:'Arunachal Pradesh', city:'Nirjuli', website:'https://www.nerist.ac.in'},
+
+  // IISc
+  {name:'Indian Institute of Science', type:'CFTI', state:'Karnataka', city:'Bengaluru', website:'https://www.iisc.ac.in'},
+
+  // IISERs
   {name:'Indian Institute of Science Education and Research, Pune', type:'CFTI', state:'Maharashtra', city:'Pune', website:'https://www.iiserpune.ac.in'},
-  {name:'Indian Statistical Institute, Kolkata', type:'CFTI', state:'West Bengal', city:'Kolkata', website:'https://www.isical.ac.in'}
+  {name:'Indian Institute of Science Education and Research, Kolkata', type:'CFTI', state:'West Bengal', city:'Kolkata', website:'https://www.iiserkol.ac.in'},
+  {name:'Indian Institute of Science Education and Research, Mohali', type:'CFTI', state:'Punjab', city:'Mohali', website:'https://www.iisermohali.ac.in'},
+  {name:'Indian Institute of Science Education and Research, Bhopal', type:'CFTI', state:'Madhya Pradesh', city:'Bhopal', website:'https://www.iiserb.ac.in'},
+  {name:'Indian Institute of Science Education and Research, Thiruvananthapuram', type:'CFTI', state:'Kerala', city:'Thiruvananthapuram', website:'https://www.iisertvm.ac.in'},
+  {name:'Indian Institute of Science Education and Research, Tirupati', type:'CFTI', state:'Andhra Pradesh', city:'Tirupati', website:'https://www.iisertirupati.ac.in'},
+  {name:'Indian Institute of Science Education and Research, Berhampur', type:'CFTI', state:'Odisha', city:'Berhampur', website:'https://www.iiserbpr.ac.in'},
+
+  // NITIE (Now IIM Mumbai)
+  {name:'National Institute of Industrial Engineering (now IIM Mumbai)', type:'CFTI', state:'Maharashtra', city:'Mumbai', website:'https://www.iimmumbai.ac.in'},
+
+  // Indian Statistical Institute
+  {name:'Indian Statistical Institute, Kolkata', type:'CFTI', state:'West Bengal', city:'Kolkata', website:'https://www.isical.ac.in'},
+
+  // Deemed Universities (Centrally Funded)
+  {name:'Rajiv Gandhi Institute of Petroleum Technology', type:'CFTI', state:'Uttar Pradesh', city:'Amethi', website:'https://www.rgipt.ac.in'},
+  {name:'Pandit Dwarka Prasad Mishra Indian Institute of Information Technology, Design and Manufacturing, Jabalpur', type:'CFTI', state:'Madhya Pradesh', city:'Jabalpur', website:'https://www.iiitdmj.ac.in'},
+  {name:'Indian Institute of Space Science and Technology', type:'CFTI', state:'Kerala', city:'Thiruvananthapuram', website:'https://www.iist.ac.in'},
+  {name:'National Institute of Food Technology Entrepreneurship and Management (NIFTEM)', type:'CFTI', state:'Haryana', city:'Kundli', website:'https://www.niftem.ac.in'},
+  {name:'National Institute of Food Technology Entrepreneurship and Management, Thanjavur (NIFTEM-T)', type:'CFTI', state:'Tamil Nadu', city:'Thanjavur', website:'https://www.niftem-t.ac.in'},
+  {name:'National Institute of Design, Ahmedabad', type:'CFTI', state:'Gujarat', city:'Ahmedabad', website:'https://www.nid.edu'},
+  {name:'National Institute of Design, Kurukshetra', type:'CFTI', state:'Haryana', city:'Kurukshetra', website:'https://www.nid.ac.in'},
+  {name:'National Institute of Design, Vijayawada', type:'CFTI', state:'Andhra Pradesh', city:'Vijayawada', website:'https://www.nid.ac.in'},
+  {name:'National Institute of Design, Jorhat', type:'CFTI', state:'Assam', city:'Jorhat', website:'https://www.nid.ac.in'},
+
+  // PEC Chandigarh
+  {name:'Punjab Engineering College (Deemed to be University)', type:'CFTI', state:'Chandigarh', city:'Chandigarh', website:'https://www.pec.ac.in'},
+
+  // Central Universities with Engineering Schools
+  {name:'Central University of Haryana', type:'CFTI', state:'Haryana', city:'Mahendragarh', website:'https://www.cuh.ac.in'},
+  {name:'Central University of Rajasthan', type:'CFTI', state:'Rajasthan', city:'Ajmer', website:'https://www.curaj.ac.in'},
+  {name:'Central University of Jharkhand', type:'CFTI', state:'Jharkhand', city:'Ranchi', website:'https://www.cuj.ac.in'},
+  {name:'Central University of Jammu', type:'CFTI', state:'Jammu and Kashmir', city:'Jammu', website:'https://www.cujammu.ac.in'},
+  {name:'Central University of Karnataka', type:'CFTI', state:'Karnataka', city:'Kalaburagi', website:'https://www.cuk.ac.in'},
+  {name:'Central University of Punjab', type:'CFTI', state:'Punjab', city:'Bathinda', website:'https://www.cup.edu.in'}
 ];
