42fac12cbeea4ba278d154054edcf14bc114e55d
RajuMurugan <rajumurugannp@gmail.com>
2025-11-10 11:43:44 +0530

    Delete iiit.js

--- iiit.js
+++
@@ -1,6 +0,0 @@
-
-const iiits = [
-  {name:'Indian Institute of Information Technology, Allahabad', type:'IIIT', state:'Uttar Pradesh', city:'Prayagraj', website:'https://www.iiita.ac.in'},
-  {name:'IIIT Design & Manufacturing, Kancheepuram', type:'IIIT', state:'Tamil Nadu', city:'Kancheepuram', website:'https://www.iiitdm.ac.in'},
-  {name:'Indian Institute of Information Technology, Guwahati', type:'IIIT', state:'Assam', city:'Guwahati', website:'https://www.iiitg.ac.in'}
-];
