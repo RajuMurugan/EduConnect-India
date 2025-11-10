a831c74284b9b614e65a24cff0d794e5f6e335e6
RajuMurugan <rajumurugannp@gmail.com>
2025-11-10 11:45:34 +0530

    Delete universities.js

--- universities.js
+++
@@ -1,5 +0,0 @@
-
-const centralUniversities = [
-  {name:'Jawaharlal Nehru University', type:'Central-University', state:'Delhi', city:'New Delhi', website:'https://www.jnu.ac.in'},
-  {name:'University of Hyderabad', type:'Central-University', state:'Telangana', city:'Hyderabad', website:'https://uohyd.ac.in'}
-];
