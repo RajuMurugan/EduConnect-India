af485ec07b24c7c88dcfd0498b721bea9e7cd6a9
RajuMurugan <rajumurugannp@gmail.com>
2025-11-10 11:45:12 +0530

    Delete state_universities.js

--- state_universities.js
+++
@@ -1,5 +0,0 @@
-
-const stateUniversities = [
-  {name:'Anna University', type:'State-University', state:'Tamil Nadu', city:'Chennai', website:'https://www.annauniv.edu'},
-  {name:'Jadavpur University', type:'State-University', state:'West Bengal', city:'Kolkata', website:'https://www.jaduniv.edu.in'}
-];
