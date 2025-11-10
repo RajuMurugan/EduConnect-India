28e757f652ddefebeead9d4b6a784822fc42aef1
RajuMurugan <rajumurugannp@gmail.com>
2025-11-10 13:59:32 +0530

    Update state_universities.js

--- data/state_universities.js
+++ data/state_universities.js
@@ -1,5 +1,6 @@
 const stateUniversities = [
-  // ANDHRA PRADESH
+  {name:'Anna University', type:'State University', state:'Tamil Nadu', city:'Chennai', website:'https://www.annauniv.edu'},
+  {name:'Jadavpur University', type:'State University', state:'West Bengal', city:'Kolkata', website:'https://www.jaduniv.edu.in'},
   {name:'Jawaharlal Nehru Technological University, Anantapur (JNTUA)', type:'State University', state:'Andhra Pradesh', city:'Anantapur', website:'https://www.jntua.ac.in'},
   {name:'Jawaharlal Nehru Technological University, Kakinada (JNTUK)', type:'State University', state:'Andhra Pradesh', city:'Kakinada', website:'https://www.jntuk.edu.in'},
   {name:'Andhra University', type:'State University', state:'Andhra Pradesh', city:'Visakhapatnam', website:'https://www.andhrauniversity.edu.in'},
