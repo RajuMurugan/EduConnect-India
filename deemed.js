7595f080548c80f6b63f7bd307e77325c34ee14c
RajuMurugan <rajumurugannp@gmail.com>
2025-11-10 11:45:24 +0530

    Delete deemed.js

--- deemed.js
+++
@@ -1,5 +0,0 @@
-
-const deemedUniversities = [
-  {name:'Vellore Institute of Technology', type:'Deemed', state:'Tamil Nadu', city:'Vellore', website:'https://vit.ac.in'},
-  {name:'SRM Institute of Science and Technology', type:'Deemed', state:'Tamil Nadu', city:'Chennai', website:'https://www.srmist.edu.in'}
-];
